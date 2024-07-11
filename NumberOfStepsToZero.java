ba046e160f004e58ea75e2bb81a2d04b4ab2b1d9
rk <grenkiat@gmail.com>
2024-07-11 15:51:44 +0800

    .

--- NumberOfStepsToZero.java
+++ NumberOfStepsToZero.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class NumberOfStepsToZero {
+public class NumberOfStepsToZero {
+    public static void main(String[] args) {
+        System.out.println(EfficientCountStepsToZero("011100"));
+        System.out.println(EfficientCountStepsToZero("1".repeat(400000)));
+    }
+
+    public static int EfficientCountStepsToZero(String S)
+    {
+        String newString = RemoveLeadingZeros(S);
+        int count = 0;
+        for (int i = newString.length() - 1; i >= 0; i--)
+        {
+            if (newString.charAt(i) == '1')
+            {
+                count+=2;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count - 1;
+    }
+
+    private static String RemoveLeadingZeros(String S)
+    {
+        char[] letter = S.toCharArray();
+        for (int i = 0; i < letter.length; i++)
+        {
+            if (letter[i] == '1')
+            {
+                break;
+            }
+            letter[i] = ' ';
+        }
+        return new String(letter).trim();
+    }
 }
